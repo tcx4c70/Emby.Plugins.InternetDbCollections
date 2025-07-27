6ea70beda0afa34057f5284b39b51bf9928225fd
Adam Tao <tcx4c70@gmail.com>
2025-07-27 20:19:51 +0800

    feat: Add CleanupInternetDbTask
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

---
+++ src/ScheduledTasks/CleanupInternetDbTask.cs
@@ -0,0 +1,70 @@
+namespace Emby.Plugins.InternetDbCollections.ScheduledTasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Emby.Plugins.InternetDbCollections.Collector;
+using Emby.Plugins.InternetDbCollections.Common;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Tasks;
+
+class CleanupInternetDbTask : IScheduledTask
+{
+    private readonly ILogger _logger;
+    private readonly ILibraryManager _libraryManager;
+
+    public CleanupInternetDbTask(ILibraryManager libraryManager)
+    {
+        _logger = Plugin.Instance.Logger;
+        _libraryManager = libraryManager;
+    }
+
+    public string Name => "Cleanup Metadata";
+
+    public string Description => "Cleanup metadata";
+
+    public string Key => $"{Plugin.Instance.Name}.CleanupInternetDb";
+
+    public string Category => Plugin.Instance.Name;
+
+    public async Task Execute(CancellationToken cancellationToken, IProgress<double> progress)
+    {
+        await Task.Yield();
+
+        _logger.Info("Start task {0}", Name);
+        progress?.Report(0.0);
+
+        var collectors = new CollectorBuilder()
+            .UseConfigs(Plugin.Instance.Configuration.Collectors)
+            .UseLogger(_logger)
+            .UseLibraryManager(_libraryManager)
+            .Build();
+        double step = collectors.Count == 0 ? 100.0 : 100.0 / collectors.Count;
+        double currentProgress = 0.0;
+        foreach (var collector in collectors)
+        {
+            try
+            {
+                await collector.InitializeAsync(cancellationToken);
+                await collector.CleanupMetadataAsync(new ProgressWithBound(progress, currentProgress, currentProgress + step), cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error while executing collector `{0}`: {1}\n{2}", collector.Name, ex.Message, ex.StackTrace);
+            }
+
+            currentProgress += step;
+        }
+
+        _logger.Info("Finish Task {0}", Name);
+        progress?.Report(100.0);
+    }
+
+    public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
+    {
+        return Enumerable.Empty<TaskTriggerInfo>();
+    }
+}

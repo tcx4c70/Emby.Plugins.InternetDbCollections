184e64ca1d744722544bf3a9f91aec830709dfa6
Adam Tao <tcx4c70@gmail.com>
2025-07-27 13:02:12 +0800

    feat(ImdbCollector): Support collection
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Collector/ImdbCollector.cs
+++ src/Collector/ImdbCollector.cs
@@ -37,13 +37,20 @@ abstract class ImdbCollector : ICollector
 
     public async Task UpdateMetadataAsync(IProgress<double> progress, CancellationToken cancellationToken = default)
     {
-        await CleanupTagsAsync(new ProgressWithBound(progress, 0, 50), true, cancellationToken);
-        await AddTagsAsync(new ProgressWithBound(progress, 50, 100), true, cancellationToken);
+        await UpdateTagsAsync(new ProgressWithBound(progress, 0, 50), cancellationToken);
+        await UpdateCollectionAsync(new ProgressWithBound(progress, 50, 100), cancellationToken);
     }
 
     public async Task CleanupMetadataAsync(IProgress<double> progress, CancellationToken cancellationToken = default)
     {
-        await CleanupTagsAsync(progress, false, cancellationToken);
+        await CleanupTagsAsync(new ProgressWithBound(progress, 0, 50), false, cancellationToken);
+        await CleanupCollectionAsync(new ProgressWithBound(progress, 50, 100), false, cancellationToken);
+    }
+
+    private async Task UpdateTagsAsync(IProgress<double> progress, CancellationToken cancellationToken = default)
+    {
+        await CleanupTagsAsync(new ProgressWithBound(progress, 0, 50), true, cancellationToken);
+        await AddTagsAsync(new ProgressWithBound(progress, 50, 100), true, cancellationToken);
     }
 
     private Task CleanupTagsAsync(IProgress<double> progress, bool onlyNotInCollection = false, CancellationToken cancellationToken = default)
@@ -100,6 +107,122 @@ abstract class ImdbCollector : ICollector
         }
 
         _logger.Info("Added tag '{0}' to {1} items", _name, items.Length);
+        progress.Report(100);
+        return Task.CompletedTask;
+    }
+
+    private async Task UpdateCollectionAsync(IProgress<double> progress, CancellationToken cancellationToken = default)
+    {
+        await CleanupCollectionAsync(new ProgressWithBound(progress, 0, 50), true, cancellationToken);
+        await AddCollectionAsync(new ProgressWithBound(progress, 50, 100), true, cancellationToken);
+    }
+
+    private Task CleanupCollectionAsync(IProgress<double> progress, bool onlyNotInCollection = false, CancellationToken cancellationToken = default)
+    {
+        var collections = _libraryManager.GetItemList(new InternalItemsQuery
+        {
+            IncludeItemTypes = new[] { nameof(BoxSet) },
+            Name = _name,
+        });
+        if (collections.Length == 0)
+        {
+            _logger.Debug("No collection with name '{0}' found, nothing to cleanup", _name);
+            progress.Report(100);
+            return Task.CompletedTask;
+        }
+        if (collections.Length > 1)
+        {
+            _logger.Warn("Found {0} collections with name '{1}', expected 1", collections.Length, _name);
+            progress.Report(100);
+            return Task.CompletedTask;
+        }
+
+        var collection = collections[0];
+        var items = _libraryManager.GetItemList(new InternalItemsQuery
+        {
+            IncludeItemTypes = new[] { nameof(Movie), nameof(Series) },
+            CollectionIds = new[] { collection.InternalId },
+        }).ToList();
+        if (onlyNotInCollection)
+        {
+            items = items
+                .Where(item => !item.ProviderIds.ContainsKey(s_providerId) || !_items.ContainsKey(item.ProviderIds[s_providerId]))
+                .ToList();
+        }
+        _logger.Info("Found {0} items need to be removed from the BoxSet '{1}'", items.Count, _name);
+
+        foreach (var (idx, item) in items.Select((item, idx) => (idx, item)))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            progress.Report(100.0 * idx / items.Count);
+            item.RemoveCollection(collection.InternalId);
+            _libraryManager.UpdateItem(item, item, ItemUpdateType.MetadataEdit);
+            _logger.Debug("Remove item '{0}' from collection '{1}' ({2}/{3})", item.Name, _name, idx + 1, items.Count);
+        }
+
+        _logger.Info("Removed {0} items from collection '{1}'", items.Count, _name);
+        progress.Report(100);
+        return Task.CompletedTask;
+    }
+
+    private Task AddCollectionAsync(IProgress<double> progress, bool onlyNotInCollection = true, CancellationToken cancellationToken = default)
+    {
+        var items = _libraryManager.GetItemList(new InternalItemsQuery
+        {
+            IncludeItemTypes = new[] { nameof(Movie), nameof(Series) },
+            AnyProviderIdEquals = _items.Keys.Select((imdbId, _) => KeyValuePair.Create(s_providerId, imdbId)).ToList(),
+        }).ToList();
+        if (onlyNotInCollection)
+        {
+            var colls = _libraryManager.GetItemList(new InternalItemsQuery
+            {
+                IncludeItemTypes = new[] { nameof(BoxSet) },
+                Name = _name,
+            });
+            if (colls.Length == 1)
+            {
+                List<BaseItem> itemsInCollection = _libraryManager.GetItemList(new InternalItemsQuery
+                {
+                    IncludeItemTypes = new[] { nameof(Movie), nameof(Series) },
+                    CollectionIds = new[] { colls[0].InternalId },
+                }).ToList();
+                items = items.Except(itemsInCollection, new BaseItemComparer()).ToList();
+            }
+            else
+            {
+                _logger.Warn("Found {0} collections with name '{1}', expected 1", colls.Length, _name);
+            }
+        }
+        _logger.Info("Found {0} items in library, start to add them to collection '{1}'", items.Count, _name);
+
+        foreach (var (idx, item) in items.Select((item, idx) => (idx, item)))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            progress.Report(100.0 * idx / items.Count);
+            item.AddCollection(_name);
+            _libraryManager.UpdateItem(item, item, ItemUpdateType.MetadataEdit);
+            _logger.Debug("Add item '{0}' to collection '{1}' ({2}/{3})", item.Name, _name, idx + 1, items.Count);
+        }
+        _logger.Info("Added {0} items to collection '{1}'", items.Count, _name);
+
+        var collections = _libraryManager.GetItemList(new InternalItemsQuery
+        {
+            IncludeItemTypes = new[] { nameof(BoxSet) },
+            Name = _name,
+        });
+        if (collections.Length == 1)
+        {
+            BaseItem collection = collections[0];
+            collection.Overview = _description;
+            _libraryManager.UpdateItem(collection, collection, ItemUpdateType.MetadataEdit);
+            _logger.Info("Updated collection '{0}' with description", _name);
+        }
+        else
+        {
+            _logger.Warn("Found {0} collections with name '{1}', expected 1", collections.Length, _name);
+        }
         progress.Report(100);
         return Task.CompletedTask;
     }
---
+++ src/Common/BaseItemComparer.cs
@@ -0,0 +1,32 @@
+namespace Emby.Plugins.InternetDbCollections.Common;
+
+using System.Collections.Generic;
+using MediaBrowser.Controller.Entities;
+
+public class BaseItemComparer : IEqualityComparer<BaseItem>
+{
+    public bool Equals(BaseItem x, BaseItem y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x is null || y is null)
+        {
+            return false;
+        }
+
+        return x.InternalId == y.InternalId;
+    }
+
+    public int GetHashCode(BaseItem obj)
+    {
+        if (obj is null)
+        {
+            return 0;
+        }
+
+        return obj.InternalId.GetHashCode();
+    }
+}
